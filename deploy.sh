89f7df4fbcf524a112969029bf4fc775e0330df6
Joe Gibbs Politz <joe.politz@gmail.com>
2016-10-03 14:57:39 -0400

    deploy to Brown ftp

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 SHORT_COMMIT=`echo $TRAVIS_COMMIT | cut -c1-7`
+VERSION=$SHORT_COMMIT
+BRANCH=$TRAVIS_BRANCH
+make
+
 tar czf build/docs.tgz build/docs
-curl --ftp-create-dirs -T build/docs.tgz -u $FTP_USER:$FTP_PASS ftp://ftp.cs.brown.edu/pyret-docs/
+curl --ftp-create-dirs -T build/docs.tgz -u $FTP_USER:$FTP_PASS ftp://ftp.cs.brown.edu/pyret-docs/$BRANCH/
 
