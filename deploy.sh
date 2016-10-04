c3150fdc4e8ae5d09f52bef7c6b5fa521c9a9c88
Joe Gibbs Politz <joe.politz@gmail.com>
2016-10-04 13:52:54 -0400

    more deploy tweaking

--- deploy.sh
+++ deploy.sh
@@ -13,9 +13,16 @@ tar czf build/docs.tgz build/docs
 # The "*" in front of dele makes the copy continue if the command fails.  This
 # makes it so new branches don't need to have someone manually make a file to
 # delete (see the -Q option in `man curl`).
+#
+# We copy the file into a version with the "-copy" suffix, and then move it
+# over, so that commands that poll on the server won't process a
+# partially-copied file.  Note that the moving commands act as if the ftp
+# command has moved into the pyret-docs/$BRANCH directory
 
 curl -u $FTP_USER:$FTP_PASS \
-     -Q "*dele pyret-docs/$BRANCH/docs.tgz"\
+     -Q "*dele pyret-docs/$BRANCH/docs-copy.tgz"\
+     -Q "-rnfr docs-copy.tgz"\
+     -Q "-rnto docs.tgz"\
      --ftp-create-dirs \
-     -T build/docs.tgz ftp://ftp.cs.brown.edu/pyret-docs/$BRANCH/docs.tgz
+     -T build/docs.tgz ftp://ftp.cs.brown.edu/pyret-docs/$BRANCH/docs-copy.tgz
 
