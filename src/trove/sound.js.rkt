c92eb571846859f0edf02574f530808cc9580683
Liye Zhong <liyezhong@dhcp-ccc-9803.redrover.cornell.edu>
2019-11-17 16:27:11 -0500

    draft

--- src/trove/sound.js.rkt
+++ src/trove/sound.js.rkt
@@ -8,41 +8,162 @@
 @(append-gen-docs
 '(module "sound"
   (path "build/phase1/trove/sound.js")
-  (fun-spec (name "overlay") (arity 2))
-  (fun-spec (name "overlay-xy") (arity 4))
-  (data-spec (name "Image") (variants) (shared))
-  (data-spec (name "Scene") (variants) (shared))
+  (fun-spec (name "makeSound") (arity 2))
+  (fun-spec (name "getSoundFromURL") (arity 1))
+  (fun-spec (name "getBufferFromURL") (arity 1))
+  (fun-spec (name "getBufferFromSound") (arity 1))
+  (fun-spec (name "denormalizeSound") (arity 1))
+  (fun-spec (name "getSoundFromAudioBuffer") (arity 1))
+  (fun-spec (name "overlay") (arity 1))
+  (fun-spec (name "concat") (arity 1))
+  (fun-spec (name "setPlaybackSpeed") (arity 2))
+  (fun-spec (name "shorten") (arity 3))
+  (fun-spec (name "getCosineWave") (arity 0))
+  (fun-spec (name "getSineWave") (arity 0))
+  (fun-spec (name "getTone") (arity 1))
+  (fun-spec (name "fade") (arity 1))
+  (fun-spec (name "removeVocals") (arity 1))
+  (data-spec (name "Sound") (variants) (shared))
+  (data-spec (name "AudioBuffer") (variants) (shared))
 ))
+@(define Sound (a-id "Sound" (xref "sound" "Sound")))
+@(define AudioBuffer (a-id "AudioBuffer" (xref "audiobuffer" "AudioBuffer")))
 
 @docmodule["sound"]{
 
   The functions in this module are used for creating, combining, and displaying
-  images.
+  sounds.
 
-  @margin-note{
-  The Pyret images library is based on the @code{images} teachpack in @emph{How to Design Programs},
-  a textbook whose pedagogy has heavily influenced Pyret's design (see
-  @hyperlink["https://www.pyret.org/pyret-code/#what-are-some-ways-the-educational-philosophy-influences-the-language"]{Why Pyret?} for more information).
-  @bold{@emph{Note:}} this original library is written in Racket, a @emph{different} language than
-  Pyret.  The examples in that file therefore do @emph{not} work with Pyret as written;
-  however, analogous examples are shown here.  If you're curious,
-  you can find documentation for the @code{images} teachpack here:
-
-  @url["http://docs.racket-lang.org/teachpack/2htdpimage.html"]
-  }
-
-  @section[#:tag "image_DataTypes"]{Data Types}
-  @type-spec["Image" (list)]{
+  @section[#:tag "sound_DataTypes"]{Data Types}
+  @type-spec["Sound" (list)]{
 
     This is the return type of many of the functions in this module; it
-    includes simple shapes, like circles and squares, and also combinations
-    or transformations of existing shapes, like rotations, overlays, and
-    scaling.
+    includes simple sounds, and also combinations or transformations of 
+    existing sounds, like overlays, concatenations, and scaling.
     }
-  @type-spec["Scene" (list)]{
+  @type-spec["AudioBuffer" (list)]{
 
-    Like an @pyret-id["Image"] but with a few special functions that crop any
-    overhanging parts of images that are placed atop them, instead of
-    stretching to accommodate.
+    The AudioBuffer represents a short audio asset residing in memory; it
+    includes sample rate, channel data, and length.
     }
+
+  @section{Basic Sounds}
+  @function[
+    "makeSound"
+            #:contract (a-arrow N (A-of (A-of N)) Sound)
+            #:return Sound   
+            #:args (list '("sample_rate" "")
+                         '("data_array" "") )]{
+              Constructs a sound with the given sample rate and data array.
+            }
+  @function[
+    "getSoundFromURL"
+            #:contract (a-arrow S Sound)
+            #:return Sound   
+            #:args (list '("path" ""))]{
+              Constructs a sound object from the given url path.
+            }
+
+  @section{Sound Buffers}
+  @function[
+    "getBufferFromURL"
+            #:contract (a-arrow S Sound)
+            #:return Sound   
+            #:args (list '("path" ""))]{
+              Constructs a sound buffer from the given url path.
+            }
+  @function[
+    "getBufferFromSound"
+            #:contract (a-arrow Sound AudioBuffer)
+            #:return Sound   
+            #:args (list '("sound" ""))]{
+              Constructs a andio buffer from the given sound.
+            }
+  @function[
+    "getSoundFromAudioBuffer"
+            #:contract (a-arrow AudioBuffer Sound)
+            #:return Sound   
+            #:args (list '("buffer" ""))]{
+              Constructs a sound from the given audio buffer.
+            }
+  @function[
+    "denormalizeSound"
+            #:contract (a-arrow AudioBuffer AudioBuffer)
+            #:return Sound   
+            #:args (list '("buffer" ""))]{
+              Denormalizes the given audio buffer and returns the result as a new audio buffer.
+            }
+  @section{Overlaying Sounds}
+  @function[
+    "overlay"
+            #:contract (a-arrow (L-of Sound) Sound)
+            #:return Sound   
+            #:args (list '("samples" ""))]{
+              Overlays all sounds in the given sample list and returns the result as a new sound.
+            }
+
+  @section{Concatenating Sounds}
+  @function[
+    "concat"
+            #:contract (a-arrow (L-of Sound) Sound)
+            #:return Sound   
+            #:args (list '("samples" ""))]{
+              Concatenates all sounds in the given sample list in order and returns the result as a new sound.
+            }
+  @section{Scaling, and Shortening Sounds}
+  @function[
+    "setPlaybackSpeed"
+            #:contract (a-arrow Sound N Sound)
+            #:return Sound   
+            #:args (list '("sample" "")
+                         '("rate" ""))]{
+              Constructs a sound with the given sample at the given rate.
+            }
+  @function[
+    "shorten"
+            #:contract (a-arrow Sound N N Sound)
+            #:return Sound   
+            #:args (list '("sample" "")
+                         '("start" "")
+                         '("end" ""))]{
+              Shortens a sound with the given start and end time.
+            }
+  @section{Starter Sound Waves and Tones}
+  @function[
+    "getCosineWave"
+            #:contract (a-arrow Sound)
+            #:return Sound   
+            #:args (list )]{
+              Constructs consine waves.
+            }
+  @function[
+    "getSineWave"
+            #:contract (a-arrow Sound)
+            #:return Sound   
+            #:args (list )]{
+              Constructs sine waves.
+            }
+  @function[
+    "getTone"
+            #:contract (a-arrow S Sound)
+            #:return Sound   
+            #:args (list '("key" ""))]{
+              Constructs a tone with the given key.
+            }
+  @section{Panning and Fading Sounds}
+  @function[
+    "fade"
+            #:contract (a-arrow Sound Sound)
+            #:return Sound   
+            #:args (list '("sound" ""))]{
+              Fades the given sound and returns the result.
+            }
+  @section{Removing Vocals}
+  @function[
+    "removeVocals"
+            #:contract (a-arrow Sound Sound)
+            #:return Sound   
+            #:args (list '("sound" ""))]{
+              Removes vocals from the given sound and returns the result.
+            }
 }
